f86aea3c5c63ad7fcd8dcddc565076a367b09218
Taha Sari <saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.com>
2022-10-31 00:56:43 +0300

    commit2-bugsfixed

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void desen(int sayi) {
-   int sayi2;
-   Scanner scanner=new Scanner(System.in);
-        if(sayi<=0){System.out.println(sayi);
-            desen(sayi+5);
-        }
 
-       System.out.println(sayi);
-       desen(sayi-5);
+    public static void desen(int number) {
+int number2=number;int i=-1;
+   if(number>-9990&&number>(i*5)) {
+       System.out.println(number);
+       desen(number - 5);
+       i++;if(number>0){
+System.out.println(number2);}
+   }
 
 
-        System.out.println("Sayı giriniz sayı eşit olmalı en yüksek değer ile");
-        sayi2= scanner.nextInt();
-        if(sayi2==sayi){System.out.println(sayi2);}
 
 
 
        }
 
 
-    public static void main(String[] args) {
 
-        desen(15);
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        int value;
+        System.out.println("Please enter a value:");
+        value= scanner.nextInt();
+        desen(value);
     }
 }
